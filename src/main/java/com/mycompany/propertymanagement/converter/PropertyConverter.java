302c96b0d85fc64cb65d49988310bdc9a74c545f
ChethanShetty563 <chethan563@gmail.com>
2023-04-06 23:23:01 +0530

    Initiating repo

---
+++ src/main/java/com/mycompany/propertymanagement/converter/PropertyConverter.java
@@ -0,0 +1,36 @@
+package com.mycompany.propertymanagement.converter;
+
+import com.mycompany.propertymanagement.dto.PropertyDTO;
+import com.mycompany.propertymanagement.entity.PropertyEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PropertyConverter {
+
+    public PropertyEntity propertyDTOtoEntity(PropertyDTO propertyDTO) {
+        PropertyEntity pe = new PropertyEntity();
+        pe.setTitle(propertyDTO.getTitle());
+        pe.setDescription(propertyDTO.getDescription());
+        pe.setDescription(propertyDTO.getDescription());
+        pe.setOwnerName(propertyDTO.getOwnerName());
+        pe.setOwnerEmail(propertyDTO.getOwnerEmail());
+        pe.setPrice(propertyDTO.getPrice());
+        pe.setAddress(propertyDTO.getAddress());
+
+        return pe;
+    }
+
+    public PropertyDTO propertyEntitytoDTO(PropertyEntity propertyEntity) {
+        PropertyDTO pdto = new PropertyDTO();
+        pdto.setId(propertyEntity.getId());
+        pdto.setTitle(propertyEntity.getTitle());
+        pdto.setDescription(propertyEntity.getDescription());
+        pdto.setDescription(propertyEntity.getDescription());
+        pdto.setOwnerName(propertyEntity.getOwnerName());
+        pdto.setOwnerEmail(propertyEntity.getOwnerEmail());
+        pdto.setPrice(propertyEntity.getPrice());
+        pdto.setAddress(propertyEntity.getAddress());
+
+        return pdto;
+    }
+}
